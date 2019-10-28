9eb511aad2b9bde25bc5db007643ba5c5c7151c4
zhuxucheng <zhuxucheng@thundersdata.onaliyun.com>
2019-10-28 09:47:20 +0800

    修改部分样式，icon位置

--- rainloop/rainloop/v/1.11.1/static/ckeditor/config.js
+++ rainloop/rainloop/v/1.11.1/static/ckeditor/config.js
@@ -4,6 +4,7 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+	config.font_names = '宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;方正小标宋/方正小标宋;'+ config.font_names ;
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
